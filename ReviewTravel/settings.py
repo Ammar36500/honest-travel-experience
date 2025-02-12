5b9666f692af8c66377ba1003ec93463884b35ff
Ammar Ahmad <Ahmadammar304@gmail.com>
2025-02-12 20:51:03 +0000

    Fixed file extensions and ignored SQLite database

--- ReviewTravel/settings.py
+++ ReviewTravel/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,5 +130,5 @@ STATICFILES_DIRS = [BASE_DIR / 'travel/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
+STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
